23081fb162de16db2b5dab0d9647dbd6ad55999a
Bartosz Borowiecki <bartosz.s.borowiecki@gmail.com>
2017-07-19 19:14:23 +0200

    Saving and loading configuration from stream

---
+++ src/Sync.Net.Tests/SyncNetConfigurationTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sync.Net.Configuration;
+using Sync.Net.TestHelpers;
+
+namespace Sync.Net.Tests
+{
+    [TestClass]
+    public class SyncNetConfigurationTests
+    {
+        [TestMethod]
+        public void SaveLoadTest()
+        {
+            var configLocalDirectory = "testLocal";
+            var configS3Bucket = "testBucket";
+            var configRegionEndpoint = RegionEndpoint.APNortheast1;
+
+            var config = new SyncNetConfiguration
+            {
+                LocalDirectory = configLocalDirectory,
+                S3Bucket = configS3Bucket,
+                RegionEndpoint = configRegionEndpoint
+            };
+
+
+            var memoryStream = new MemoryStream();
+            config.Save(memoryStream);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            var loadedConfiguration = SyncNetConfiguration.Load(memoryStream);
+            Assert.AreEqual(configLocalDirectory, loadedConfiguration.LocalDirectory);
+            Assert.AreEqual(configS3Bucket, loadedConfiguration.S3Bucket);
+            Assert.AreEqual(configRegionEndpoint, loadedConfiguration.RegionEndpoint);
+        }
+    }
+}
--- src/Sync.Net/Configuration/SyncNetConfiguration.cs
+++ src/Sync.Net/Configuration/SyncNetConfiguration.cs
@@ -1,21 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using Amazon;
+using Sync.Net.IO;
 
 namespace Sync.Net.Configuration
 {
+    [DataContract]
     public class SyncNetConfiguration
     {
+        private static readonly DataContractSerializer Serializer;
+
+        [DataMember]
+        public string RegionEndpointSystemName { get; private set; }
+
         public SyncNetConfiguration()
         {
             RegionEndpoint = RegionEndpoint.USEast1;
         }
 
+        static SyncNetConfiguration()
+        {
+            Serializer = new DataContractSerializer(typeof(SyncNetConfiguration));
+        }
+
+        [DataMember]
         public string LocalDirectory { get; set; }
+
+        [DataMember]
         public string S3Bucket { get; set; }
-        public RegionEndpoint RegionEndpoint { get; set; }
+
+        public RegionEndpoint RegionEndpoint
+        {
+            get { return RegionEndpoint.GetBySystemName(RegionEndpointSystemName); }
+            set { RegionEndpointSystemName = value.SystemName; }
+        }
+
+        public void Save(Stream stream)
+        {
+            
+            Serializer.WriteObject(stream, this);
+        }
+
+        public static SyncNetConfiguration Load(Stream stream)
+        {
+            return Serializer.ReadObject(stream) as SyncNetConfiguration;
+        }
     }
 }
