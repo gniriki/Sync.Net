fc76c22d24525f0a618a6b0f366a75e732bec770
Bartosz Borowiecki <bartosz.s.borowiecki@gmail.com>
2017-08-07 22:36:16 +0200

    Scrolling log text box

--- src/Sync.Net.UI/MainWindow.xaml.cs
+++ src/Sync.Net.UI/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Threading;
+using Sync.Net.UI.ViewModels;
 
 namespace Sync.Net.UI
 {
@@ -9,7 +12,22 @@ namespace Sync.Net.UI
     {
         public MainWindow()
         {
+            DataContextChanged += MainWindow_DataContextChanged;
             InitializeComponent();
+        }
+
+        private void MainWindow_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var mainWindowViewModel = e.NewValue as MainWindowViewModel;
+
+            if (mainWindowViewModel != null)
+                mainWindowViewModel.PropertyChanged += MainWindow_PropertyChanged;
+        }
+
+        private void MainWindow_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(MainWindowViewModel.Log))
+                textBox.Dispatcher.Invoke(() => textBox.ScrollToEnd());
         }
     }
 }
