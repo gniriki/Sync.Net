9e4a43bf75c3a9ec58014040d1759a5acc662203
gniriki <gniriki@gmail.com>
2017-10-29 12:04:46 +0100

    Added ManualTaskQueue

---
+++ src/Sync.Net/Processing/ManualTaskQueue.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Sync.Net.Processing
+{
+    public class ManualTaskQueue : TaskQueue
+    {
+        private readonly Queue<ITask> _queue = new Queue<ITask>();
+
+        public override void Queue(ITask task)
+        {
+            _queue.Enqueue(task);
+        }
+
+        public void ExecuteNextTask()
+        {
+            Execute(_queue.Dequeue());
+        }
+
+        private void Execute(ITask task)
+        {
+            OnTaskStarting(task);
+            task.Execute();
+            OnTaskCompleted(task);
+        }
+
+        public override int Count => _queue.Count;
+    }
+}
